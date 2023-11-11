b5d927fce34299a3852ba2a7ceebca4adb47ad37
Maalim98 <ashrafzani585@gmail.com>
2023-11-11 18:44:11 +0300

    delete folders

--- 0x03-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotificationItem({ type, value, html }) {
-  if (value) {
-    return <li data-notification-type={type}>{value}</li>;
-  } else {
-    return <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>;
-  }
-}
